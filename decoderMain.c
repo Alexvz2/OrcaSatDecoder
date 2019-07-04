df7591a792758a75e6cc0c028c2f60d8a6878d5e
Alexvz2 <alexvzvz@yahoo.com>
2019-07-04 10:23:17 -0700

    Ground Station Specs.

--- decoderMain.c
+++ decoderMain.c
@@ -4,9 +4,12 @@
  * will be represented in terms of 1's and 0's
  * 
  * Still requiring more information and sample data this is the bare structure.
- *
- * 
- *
+ * Frequency: 437305 kHz
+ * Modulation GFSK
+ * Bandwidth: 15kHz
+ * CallSign: 
+ * Link Layer:
+ * Baude Rate:
  * Alex Vazquez.  07/04/2019
  */
 
